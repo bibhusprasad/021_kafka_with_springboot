f96cac2444360599e64d57c618cb0157f87e5de6
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2024-07-24 18:05:36 +0530

    First commit

---
+++ src/main/java/com/bibhu/learn/kafka_with_springboot/KafkaWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bibhu.learn.kafka_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibhu/learn/kafka_with_springboot/KafkaWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibhu.learn.kafka_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
